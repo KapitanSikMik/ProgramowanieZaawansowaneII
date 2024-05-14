2d115c1112ca7829ece710046cba4d8c4ccc89d9
KapitanSikMik <Kapitansikmik@gmail.com>
2024-05-14 19:44:02 +0200

    Zad 5 moq 1 upload

---
+++ Zad 5 - Moq1/ZadanieMoq/ZadanieMoq.Tests/MyModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadanieMoq.Tests
+{
+    public class MyModule
+    {
+        private readonly ISomeService _someService;
+
+        public MyModule(ISomeService someService)
+        {
+            _someService = someService;
+        }
+
+        public string ProcessData()
+        {
+            var data = _someService.GetData();
+            return $"Processed: {data}";
+        }
+    }
+}
